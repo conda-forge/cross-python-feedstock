e9f1fe90157bfcbfba1e42ae3f3c4157a030b704
Isuru Fernando <isuruf@gmail.com>
2020-09-14 18:23:59 -0500

    Activate at the end

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 mkdir -p ${PREFIX}/etc/conda/activate.d
 mkdir -p ${PREFIX}/etc/conda/deactivate.d
-cp "${RECIPE_DIR}"/activate-${PKG_NAME}.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
-cp "${RECIPE_DIR}"/deactivate-${PKG_NAME}.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
+cp "${RECIPE_DIR}"/activate-${PKG_NAME}.sh ${PREFIX}/etc/conda/activate.d/activate-z-${PKG_NAME}.sh
+cp "${RECIPE_DIR}"/deactivate-${PKG_NAME}.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-z-${PKG_NAME}.sh
