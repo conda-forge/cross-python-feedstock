528d3cd5ec1e3cdabb044123509f4a333ffce192
Isuru Fernando <isuruf@gmail.com>
2020-09-14 15:45:17 -0500

    set PYTHONPATH

--- recipe/activate-cross-python.sh
+++ recipe/activate-cross-python.sh
@@ -7,7 +7,14 @@ if [[ "${CONDA_BUILD:-0}" == "1" && "${CONDA_BUILD_STATE}" != "TEST" ]]; then
       --sysconfigdata-file $PREFIX/lib/python$PY_VER/${_CONDA_PYTHON_SYSCONFIGDATA_NAME}.py
   cp $BUILD_PREFIX/venv/cross/bin/python $PREFIX/bin/python
   rm -rf $BUILD_PREFIX/venv/cross
-  if [[ -f $PREFIX/lib/python$PY_VER/site-packages/numpy/distutils/site.cfg ]]; then
-    rsync -a -I $PREFIX/lib/python$PY_VER/site-packages/numpy/ $BUILD_PREFIX/lib/python$PY_VER/site-packages/numpy/
+  rsync -a -I $PREFIX/lib/python$PY_VER/site-packages/ $BUILD_PREFIX/lib/python$PY_VER/site-packages/
+  rm -rf $PREFIX/lib/python$PY_VER/site-packages
+  rm -rf $BUILD_PREFIX/venv/lib/python$PY_VER/site-packages
+  ln -s $BUILD_PREFIX/lib/python$PY_VER/site-packages $BUILD_PREFIX/venv/lib/python$PY_VER/site-packages
+  sed -i.bak "s@$BUILD_PREFIX/venv/lib@$BUILD_PREFIX/venv/lib', '$BUILD_PREFIX/venv/lib/python$PY_VER/site-packages@g" $PYTHON
+
+  if [[ "${PYTHONPATH}" != "" ]]; then
+    _CONDA_BACKUP_PYTHONPATH=${PYTHONPATH}
   fi
+  export PYTHONPATH=$BUILD_PREFIX/venv/lib/python$PY_VER/site-packages
 fi
